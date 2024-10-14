5688617b97d830997f6220c0a68f5af18b62659f
Ramon Martinez <rampa@encomix.org>
2024-10-14 22:39:30 +0200

    cleanup

--- build_id.mk
+++
@@ -1,7 +0,0 @@
-DEMISTIFYPATH=DeMiSTify
-include $(DEMISTIFYPATH)/site.mk
-
-.PHONY: build_id.v
-build_id.v: mist/build_id_verilog.tcl
-	$(Q13)/quartus_sh -t mist/build_id_verilog.tcl
-
--- defs_demistify.v
+++
@@ -1,4 +0,0 @@
-///////////////   GLOBAL DEFINES   ////////////////
-	
-`define GUEST_TOP guest_top	// substitute guest_top by guest Mist top module name		
-
